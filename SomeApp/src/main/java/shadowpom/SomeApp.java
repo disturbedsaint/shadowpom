2cf839cabd7e32998f88aefb09b50c86def31252
Dan Schmidt <dcschmid@gmail.com>
2019-09-10 14:00:58 -0400

    Demonstrate the bug

---
+++ SomeApp/src/main/java/shadowpom/SomeApp.java
@@ -0,0 +1,7 @@
+package shadowpom;
+
+public class SomeApp {
+  public static void main(String[] args) {
+    System.out.println(SomeLib1.isNullOrEmpty("foo"));
+  }
+}
---
+++ SomeLib1/src/main/java/shadowpom/SomeLib1.java
@@ -0,0 +1,9 @@
+package shadowpom;
+
+import shadowpom.internal.com.google.common.base.Strings;
+
+public class SomeLib1 {
+  public static boolean isNullOrEmpty(String string) {
+    return Strings.isNullOrEmpty(string);
+  }
+}
